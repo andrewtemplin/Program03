ff05c56f39459f1d94101113d4067641b9e8854c
Andrew Templin <andrew@Andrews-MacBook-Air.local>
2020-10-07 13:16:28 -0500

    initial commit

---
+++ src/edu/ilstu/Cypher.java
@@ -0,0 +1,30 @@
+package edu.ilstu;
+
+import java.util.Scanner;
+
+public class Cypher {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		
+		char cipherText [] = {'c','s','i','m','u','y','l','a','n','g','p','w','z','f','r','x','b','v','h','d','t','e','j','q','k','o'};
+		char plainText [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		String quit3 = "Thank you for using the Normal Encrypter!";
+		
+		System.out.println("Welcome to the Normal Encrypter!");
+		System.out.println();
+		System.out.println("You may: ");
+		System.out.println("	1.) Encrpyt a message");
+		System.out.println(" 	2.) Decrpyt a message");
+		System.out.println(" 	3.) Quit");
+		System.out.println();
+		System.out.println("Please enter choice (1, 2, or 3): ");
+		String input = scan.nextLine();
+		String choice = "";
+		
+		
+
+	}
+
+}
